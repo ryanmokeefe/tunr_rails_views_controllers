3ba8c98d74236971677b7c15a7f4b15e628bdd88
Andy <andrew.sunglae.kim@gmail.com>
2015-07-26 18:01:18 -0400

    add controller actions to artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,41 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    @artists = Artist.all.order(:id).reverse
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(params[:artist])
+    @artist.update( date_taken: Time.now.strftime("%B %d, %Y") )
+    if @artist.save
+      redirect_to "/artists/#{@artist.id}"
+    else
+      redirect_to "/posts/new"
+    end
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params[:artist])
+    redirect_to "/artists/#{@artist.id}"
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to "/"
+  end
+
+
 end
